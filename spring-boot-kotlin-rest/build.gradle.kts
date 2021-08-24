524385a7eecf613d3daf95f76ab719304d952631
Arho Huttunen <arho.huttunen@iki.fi>
2021-08-24 18:04:35 +0300

    Bump jvm from 1.5.21 to 1.5.30

--- spring-boot-kotlin-rest/build.gradle.kts
+++ spring-boot-kotlin-rest/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.30"
     kotlin("plugin.spring") version "1.5.30"
     id("org.springframework.boot") version "2.5.4"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
